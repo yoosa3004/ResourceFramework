efce8a75ef9b47b2df6a2f7ba40f0ef67002837b
Hyunndy <yoosa3004@naver.com>
2023-04-17 08:24:58 +0900

    Initial Commit

---
+++ ResourceFramework/ResourceFramework.h
@@ -0,0 +1,18 @@
+//
+//  ResourceFramework.h
+//  ResourceFramework
+//
+//  Created by hyunndy on 2023/04/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResourceFramework.
+FOUNDATION_EXPORT double ResourceFrameworkVersionNumber;
+
+//! Project version string for ResourceFramework.
+FOUNDATION_EXPORT const unsigned char ResourceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResourceFramework/PublicHeader.h>
+
+
